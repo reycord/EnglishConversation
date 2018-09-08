c1f4bf426fa81384912eb75d08641cfa767ef2fe
thihue <annhien61196@gmail.com>
2018-09-08 10:01:15 +0700

    add login javascript

--- vendors_login/js/main.js
+++ vendors_login/js/main.js
@@ -3,18 +3,18 @@
     "use strict";
 
 
-    // /*==================================================================
-    // [ Focus Contact2 ]*/
-    // $('.input100').each(function(){
-    //     $(this).on('blur', function(){
-    //         if($(this).val().trim() != "") {
-    //             $(this).addClass('has-val');
-    //         }
-    //         else {
-    //             $(this).removeClass('has-val');
-    //         }
-    //     })    
-    // })
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
   
   
     // /*==================================================================
